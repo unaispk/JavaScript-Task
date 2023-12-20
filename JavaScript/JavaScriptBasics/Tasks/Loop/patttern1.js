06ea1be7b1b6dd090c8216369eb906b1f07941b4
muhammed unais pk <127509037+unaispk@users.noreply.github.com>
2023-12-20 21:47:59 +0530

    Reverse Pattern
    Code for Reverse Pattern Added

--- JavaScript/JavaScriptBasics/Tasks/Loop/patttern1.js
+++ JavaScript/JavaScriptBasics/Tasks/Loop/patttern1.js
@@ -9,16 +9,18 @@
 // for(let i=1; i<=5; i++){
 //     for(let j=1; j<=i; j++){
 //         console.log("* ");
-        
+
 //     }
 
 //     console.log("\n");
 // }
 
-for(let i=5; i>=5; i--){    
+// reverse pattern
+
+for (let i = 1; i <= 5; i++) {
     let a = "";
-    for(let j=1; j<=i; j++){
-       a += "* ";   //a=a+*   
+    for (let j = 5; j >= i; j--) {
+        a += "* ";   //a=a+*   
     }
     console.log(a);
 }
