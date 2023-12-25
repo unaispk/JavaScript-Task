903ccb28846159a39488a645af1397d98667eb12
muhammed unais pk <127509037+unaispk@users.noreply.github.com>
2023-12-25 21:07:03 +0530

    Basic Calculator Task updation
    Basic Calculator Task :
    Updation on index.html and style.css files.

--- JavaScript/JavaScriptBasics/Tasks/DOM Tasks/BasicCalculator/script.js
+++ JavaScript/JavaScriptBasics/Tasks/DOM Tasks/BasicCalculator/script.js
@@ -1,7 +1,62 @@
+const buttonClick = (value) => screen.value += value;
+// const deleteBtn = () => screen.value = screen.value.toString().slice(0,-1);
+// const equalBtn = () => screen.value = eval(screen.value);
+
 let screen = document.getElementById('screen');
 
-const buttonClick = (value) => screen.value += value;
-const allClear = () => screen.value = '' ;
-const deleteBtn = () => screen.value = screen.value.toString().slice(0,-1);
+const onlyAllow = (event) => {
+    const allowedChars = /[0-9+\-*/.=]|Backspace|Delete|Enter/;
+    return event.key.match(allowedChars);
+};
 
-const equalBtn = () => screen.value = eval(screen.value);
+const updateScreen = (value) => {
+    if (!isNaN(value) || value === '.' || ['+', '-', '*', '/'].includes(value)) {
+        screen.value += value;
+    }
+};
+
+const allClear = () => {
+    screen.value = '';
+};
+
+const deleteBtn = () => {
+    screen.value = screen.value.slice(0, -1);
+};
+
+const equalBtn = () => {
+    try {
+        screen.value = eval(screen.value);
+    } catch (error) {
+        screen.value = 'Error';
+    }
+};
+
+screen.addEventListener('input', () => {
+    screen.value = screen.value.replace(/[^0-9+\-*/.=]/g, '');
+});
+
+document.addEventListener('keypress', (event) => {
+    if (event.key.match(/[0-9+\-*/.=]/)) {
+        updateScreen(event.key);
+    }
+});
+
+document.addEventListener('keydown', (event) => {
+    if(event.key === 'Backspace') {
+        deleteBtn();
+    }
+});
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        equalBtn();
+        event.preventDefault(); // Prevent the default behavior of the 'Enter' key
+    }
+});
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === '=') {
+        equalBtn();
+        event.preventDefault(); 
+    }
+});
